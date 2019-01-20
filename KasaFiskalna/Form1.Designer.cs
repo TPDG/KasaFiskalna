ef1b9409052ebeef02c22f1a85be5ad68aeafdf3
Kamil Kijańczuk <2kamcio1@gmail.com>
2019-01-20 21:01:54 +0100

    Final app

--- KasaFiskalna/Form1.Designer.cs
+++ KasaFiskalna/Form1.Designer.cs
@@ -40,17 +40,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -58,11 +47,14 @@
             this.button4 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label14 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -179,116 +171,6 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.richTextBox1);
-            this.groupBox2.Controls.Add(this.label13);
-            this.groupBox2.Controls.Add(this.label12);
-            this.groupBox2.Controls.Add(this.label11);
-            this.groupBox2.Controls.Add(this.label10);
-            this.groupBox2.Controls.Add(this.label9);
-            this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(258, 32);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(234, 300);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Paragon";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(6, 273);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(54, 17);
-            this.label13.TabIndex = 9;
-            this.label13.Text = "label13";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(6, 245);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(54, 17);
-            this.label12.TabIndex = 8;
-            this.label12.Text = "label12";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(6, 194);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(54, 17);
-            this.label11.TabIndex = 7;
-            this.label11.Text = "label11";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 167);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(54, 17);
-            this.label10.TabIndex = 6;
-            this.label10.Text = "label10";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 144);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(46, 17);
-            this.label9.TabIndex = 5;
-            this.label9.Text = "label9";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 119);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 17);
-            this.label8.TabIndex = 4;
-            this.label8.Text = "label8";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 94);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 3;
-            this.label7.Text = "label7";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 71);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "label6";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 49);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "label5";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "label4";
-            // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(13, 305);
@@ -356,14 +238,43 @@
             this.label14.TabIndex = 6;
             this.label14.Text = "Ilość:";
             // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 245);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 17);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 273);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 17);
+            this.label13.TabIndex = 9;
+            this.label13.Text = "label13";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Location = new System.Drawing.Point(258, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(234, 300);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Paragon";
+            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(112, 21);
+            this.richTextBox1.Location = new System.Drawing.Point(6, 21);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(116, 269);
+            this.richTextBox1.Size = new System.Drawing.Size(228, 221);
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
@@ -385,10 +296,10 @@
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -404,7 +315,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -413,19 +323,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- KasaFiskalna/Form1.cs
+++ KasaFiskalna/Form1.cs
@@ -13,23 +13,24 @@ namespace KasaFiskalna
     public partial class Form1 : Form
     {
         int pozycja = 0;
-        double[] ceny = new double[8];
-        double cena = 0, zaplata = 0, reszta = 0;
-        bool b2click = false, b1click = false, test = false;
+        decimal[] ceny = new decimal[50];
+        decimal cena = 0, zaplata = 0, reszta = 0;
+        bool test = false;
 
         public Form1()
         {
             InitializeComponent();
             Clean();
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            b1click = true;
-            Calculate();
+            ceny[pozycja] = Convert.ToDecimal(textBox2.Text) * Convert.ToDecimal(textBox4.Text);
+            if (pozycja < 50) { pozycja++; button1.Enabled = true; } else { button1.Enabled = false; }
+            if (pozycja > 0) { button2.Enabled = true; }
+            richTextBox1.Text += textBox1.Text + " - " + Convert.ToDouble(textBox4.Text) * Convert.ToDouble(textBox2.Text) + ";\n";
             cena = 0;
-            foreach (double element in ceny)
+            foreach (decimal element in ceny)
             {
                 cena += element;
             }
@@ -39,7 +40,7 @@ namespace KasaFiskalna
 
         private void button4_Click(object sender, EventArgs e)
         {
-            zaplata = Convert.ToDouble(textBox3.Text);
+            zaplata = Convert.ToDecimal(textBox3.Text);
             reszta = zaplata - cena;
             label13.Text = "Reszta: " + reszta + "zł.";
         }
@@ -61,19 +62,31 @@ namespace KasaFiskalna
 
         private void pomocToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Program Kasa Fiskalna.\n\n Program przyjmuje liczby w formacie np. '12,00'.\n Podaj nazwę przedmiotu i jego cenę. Wciśnij przycisk 'Dodaj'. \n Jeśli chcesz usunąć przedmiot wciśnij 'Usuń ostatni'.\n Aby wydać reszte podaj wpłaconą kwotę i wciśnij przycisk 'Reszta'.\n\nAutor: Kamil Kijańczuk");
+            MessageBox.Show("Program Kasa Fiskalna.\n\n Program przyjmuje liczby w formacie np. '12,00'.\n Podaj nazwę przedmiotu, jego cenę oraz ilość. Wciśnij przycisk 'Dodaj'. \n Jeśli chcesz usunąć przedmiot wciśnij 'Usuń ostatni'.\n Aby wydać reszte podaj wpłaconą kwotę i wciśnij przycisk 'Reszta'.\n\nAutor: Kamil Kijańczuk");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            b2click = true;
-            Calculate();
+            if (pozycja > 0) { pozycja--; }
+            if (pozycja <= 0) { button2.Enabled = false; }
             if (test == false)
             {
                 if (pozycja >= 0) { cena -= ceny[pozycja]; ceny[pozycja] = 0; }
                 label12.Text = "Do zapłaty: " + cena + "zł.";
             }
             test = false;
+
+            var last = richTextBox1.Text.LastIndexOf(";");
+            if (last > 0)
+            {
+                richTextBox1.Text = richTextBox1.Text.Substring(0, last - 1);
+                var beforelast = richTextBox1.Text.LastIndexOf(";");
+                richTextBox1.Text = richTextBox1.Text.Substring(0, beforelast + 1);
+            }
+            else
+            {
+                richTextBox1.Text = "";
+            }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -81,41 +94,14 @@ namespace KasaFiskalna
 
         }
 
-        private void Calculate()
-        {
-                try
-                {
-                    switch (pozycja)
-                    {
-                        case 0: { if (b1click == true) { b1click = false; b2click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label4.Text = textBox1.Text + " - " + ceny[pozycja]; pozycja++; button2.Enabled = true; } break; }
-                        case 1: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label5.Text = textBox1.Text + " - " + ceny[pozycja]; pozycja++;  } if (b2click == true) { b2click = false; label4.Text = ""; pozycja--; button2.Enabled = false; } break; }
-                        case 2: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label6.Text = textBox1.Text + " - " + textBox2.Text; pozycja++;  } if (b2click == true) { b2click = false; label5.Text = ""; pozycja--;  } break; }
-                        case 3: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label7.Text = textBox1.Text + " - " + textBox2.Text; pozycja++;  } if (b2click == true) { b2click = false; label6.Text = ""; pozycja--;  } break; }
-                        case 4: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label8.Text = textBox1.Text + " - " + textBox2.Text; pozycja++;  } if (b2click == true) { b2click = false; label7.Text = ""; pozycja--;  } break; }
-                        case 5: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label9.Text = textBox1.Text + " - " + textBox2.Text; pozycja++;  } if (b2click == true) { b2click = false; label8.Text = ""; pozycja--;  } break; }
-                        case 6: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label10.Text = textBox1.Text + " - " + textBox2.Text; pozycja++;  } if (b2click == true) { b2click = false; label9.Text = ""; pozycja--;  } break; }
-                        case 7: { if (b1click == true) { b1click = false; ceny[pozycja] = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text); label11.Text = textBox1.Text + " - " + textBox2.Text; pozycja++; button1.Enabled = false; } if (b2click == true) { b2click = false; label10.Text = ""; pozycja--;  } break; }
-                        case 8: { if (b2click == true) { b1click = false; b2click = false; label11.Text = ""; pozycja--; button1.Enabled = true; } break; }
-                        default: { b1click = false; b2click = false; break; }
-                    }
-                }
-                catch (FormatException) { MessageBox.Show("Niepoprawny format. Spróbuj liczb z przecinkiem."); test = true; }
-        }
-
         private void Clean() {
             textBox1.Text = "";
             textBox2.Text = "";
             textBox3.Text = "";
-            label4.Text = "";
-            label5.Text = "";
-            label6.Text = "";
-            label7.Text = "";
-            label8.Text = "";
-            label9.Text = "";
-            label10.Text = "";
-            label11.Text = "";
+            richTextBox1.Text = "";
             label12.Text = "";
             label13.Text = "";
+            textBox4.Text = "1";
             int i = 0;
             foreach (int element in ceny)
             {
